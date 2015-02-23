57450b5e96e636841aa0ae825489cf1f29ee6eb9
Ryan Zahm <zahmryan@msu.edu>
2015-02-22 23:31:18 -0500

    Embed Ruby in Sinatra

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 ruby '2.1.5'
 
 gem 'sinatra', '~> 1.4.5'
+gem 'holidapi'
+
+gem 'rack', '1.5.2'
--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
+require 'holidapi'
 
 class MyWebApp < Sinatra::Base
-	get '/' do
-		erb :"index#{ rand(3) + 1 }"
-	end
+  get '/' do
+    erb :index
+  end
 end
